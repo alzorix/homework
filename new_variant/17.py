e7b8a21d6202ca4bb5092f3a2d4986bed1df4662
alzorix <aediferix@gmail.com>
2025-01-08 13:52:00 +0000

    homeworking

--- new_variant/17.py
+++ new_variant/17.py
@@ -1,12 +1,12 @@
 data = list()
-max_ysl = -float("inf")
+max_ysl = 0
 with open("17.txt") as file:
     line = file.readline()
     while line !="":
         data.append(line)
         if len(str(abs(int(line)))) == 5 and line[-3::] == "43":
+            print(1)
             max_ysl = max(max_ysl,int(line))
-        else:
-            print(line[-3::])
+
         line = file.readline()
 print(max_ysl)
