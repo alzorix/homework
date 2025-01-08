8e2717f3427a2ebc7b82387640005e39c2cb617c
alzorix <aediferix@gmail.com>
2025-01-08 13:53:00 +0000

    homeworking

--- new_variant/17.py
+++ new_variant/17.py
@@ -1,12 +1,12 @@
 data = list()
 max_ysl = 0
 with open("17.txt") as file:
-    line = file.readline()
+    line = file.readline().strip()
     while line !="":
         data.append(line)
-        if len(str(abs(int(line)))) == 5 and line[-3::] == "43":
+        if len(str(abs(int(line)))) == 5 and line[-2::] == "43":
             print(1)
             max_ysl = max(max_ysl,int(line))
 
-        line = file.readline()
+        line = file.readline().strip()
 print(max_ysl)
