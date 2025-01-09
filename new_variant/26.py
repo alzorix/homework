8b5715847a71d49c6e51f1cf78be7b9479ccaa5f
alzorix <sahasdenivovpro@mail.ru>
2025-01-10 01:10:39 +0300

    homework

--- new_variant/26.py
+++ new_variant/26.py
@@ -22,6 +22,7 @@ with (open("26.txt") as file):
 ansewer = list() # ID и максимальное количество решенных задач
 tasks_big_data = list(tasks_big_data)
 tasks_big_data.sort()
+
 for ID in data.keys():
 
     nice_tasks = 0
@@ -30,18 +31,27 @@ for ID in data.keys():
     tasks = list(tasks)
     tasks.sort()
     last_task = -1
+    kolvo = 0
     for current_task in tasks:
-        kolvo =0
         if last_task == -1:
             last_task = current_task
+            kolvo+=1
         else:
             if int(tasks_big_data.index(int(last_task))) +1 == int(tasks_big_data.index(int(current_task))):
+                last_task = current_task
+
                 kolvo+=1
+
             else:
+
                 nice_tasks = max(nice_tasks,kolvo)
                 kolvo = 0
-    ansewer.append((nice_tasks,int(ID)))
+
+    #ansewer.append((nice_tasks,len(tasks),int(ID)))
+    ansewer.append((nice_tasks,-int(ID)))
 
 ansewer.sort()
-print(max(ansewer))
-
+print(ansewer)
+print(ansewer[-1])
+#44996  148
+#(148, 149, 46370), (148, 296, 44996), (148, 296, 46376)
